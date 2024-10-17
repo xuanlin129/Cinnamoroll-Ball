c649690d039536cf3bf983dc57b21b8248d19642
Xuan <127477394+xuanlin129@users.noreply.github.com>
2024-10-17 10:54:58 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const getNumber = document.querySelector(".getNumber");
+const paper = document.querySelector(".paper");
+
+let tl = gsap.timeline();
+
+getNumber.addEventListener("click", function () {
+  this.classList.add("active");
+  tl.to(".paper", {
+    height: "100%",
+    rotateX: 0,
+    duration: 1.5,
+  });
+  tl.to(".paper", {
+    zIndex: 9,
+    top: "25%",
+    scale: 1.3,
+    duration: 0.5,
+  });
+});
